6f3e5bb0912294df813532f8e6f5b351163abdb0
nguyenthanh.fds <thanhnv@fds.vn>
2019-01-07 23:15:37 +0700

    v6: user and ref user vs events

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
 const { buildSchema } = require('graphql')
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const app = express()
 
 const Event = require('./models/event')
+const User = require('./models/user')
 
 app.use(bodyParser.json())
 
@@ -25,11 +27,22 @@ app.use('/graphql',
                 date: String!
             }
 
+            type User {
+                _id: ID!
+                email: String!,
+                password: String
+            }
+
             input EventInput {
                 title: String!
                 description: String!
                 price: Float!
                 date: String!
+            }
+
+            input UserInput {
+                email: String!
+                password: String!
             }
 
             type RootQuery {
@@ -38,6 +51,7 @@ app.use('/graphql',
 
             type RootMutation {
                 crateEvent(eventInput: EventInput): Event
+                createUser(userInput: UserInput): User
             }
 
             schema {
@@ -50,7 +64,7 @@ app.use('/graphql',
                 return Event.find().then(events => {
                     return events.map(event => {
                         console.log(eventMapped(event))
-                        // return { ...event._doc, _id: event._doc.id }
+                        return { ...event._doc, _id: event._doc.id }
                         return eventMapped(event)
                     })
                 }).catch(err => {
@@ -62,16 +76,55 @@ app.use('/graphql',
                     title: args.eventInput.title,
                     description: args.eventInput.description,
                     price: +args.eventInput.price,
-                    date: new Date(args.eventInput.date)
+                    date: new Date(args.eventInput.date),
+                    creator: '5c337a9cea77d22706eb1a22'
                 })
-                return event.save().then(result => {
-                    console.log(result)
-                    // not run return { ...result._doc, _id: result._doc.id }
-                    return eventMapped(result)
-                }).catch(err => {
-                    console.log(err)
-                    throw err
+                let createdEvent;
+                return event
+                    .save()
+                    .then(result => {
+                        createdEvent = { ...result._doc, _id: result._doc.id }
+                        return User.findById('5c337a9cea77d22706eb1a22')
+                    })
+                    .then(user => {
+                        if (user) {
+                            user.createdEvents.push(event)
+                            return user.save()
+                        } else {
+                            throw new Error('User is not exits.')
+                        }
+                    })
+                    .then(result => {
+                        return createdEvent
+                    })
+                    .catch(err => {
+                        console.log(err)
+                        throw err
+                    })
+            },
+            createUser: (args) => {
+                return User.findOne({ email: args.userInput.email })
+                .then(user => {
+                    if (user) {
+                        throw new Error('User exits already.')
+                    }
+                    return bcrypt.hash(args.userInput.password, 12)
+                        .then(hashedPassword => {
+                            const user = new User({
+                                email: args.userInput.email,
+                                password: hashedPassword
+                            })
+                            return user.save()
+                        })
+                        .then(result => {
+                            return { ...result._doc, password: null, _id: result._doc.id }
+                        })
+                        .catch(err => {
+                            throw err
+                        })
                 })
+                
+
             }
         },
         graphiql: true
