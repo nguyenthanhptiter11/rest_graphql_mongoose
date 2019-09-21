ca225fd18e3efd526d50bd709640a26eb12f766c
ThanhNV <nguyenthanh.ptiter@gmail.com>
2019-09-21 16:05:49 +0700

    added authen by jwt

--- grapql/schema/index.js
+++ grapql/schema/index.js
@@ -39,16 +39,23 @@ input UserInput {
     password: String!
 }
 
+type AuthData {
+    userId: ID!
+    token: String!
+    tokenExpiration: Int!
+}
+
 type RootQuery {
     events: [Event!]!
     bookings: [Booking!]!
+    login(email: String!, password: String!): AuthData!
 }
 
 type RootMutation {
     createEvent(eventInput: EventInput): Event
     createUser(userInput: UserInput): User
     bookEvent (eventId: ID!): Booking!
-    cancelBooking(bookingId: ID!): Event!
+    cancelBooking(bookingId: ID!): Event! 
 }
 
 schema {
